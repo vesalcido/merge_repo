e765d5005989986760516f1fc2d3c78876641e65
vesalcido <vesalcido@ucsd.edu>
2025-04-28 16:22:31 -0700

    Made a change to discountPrices to pass a js test

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -25,6 +25,9 @@ function sumValues(num1, num2, add) {
  * @returns An array of each price's new price, after the discount is applied. Or false, if prices array is empty.
  */
 function discountPrices(prices, discount) {
+    if (!Array.isArray(prices) || typeof discount !== 'number') {
+        return false;
+    }
     const discounted = []
     const length = prices.length;
     let discountedPrice = 0
